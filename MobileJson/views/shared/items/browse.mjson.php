9eaba9a3a4833cf971f5fa451be70431fed5d597
Erin Bell <ebellempire@gmail.com>
2013-05-17 11:28:13 -0400

    Make sure item is public and has location data

--- MobileJson/views/shared/items/browse.mjson.php
+++ MobileJson/views/shared/items/browse.mjson.php
@@ -8,37 +8,29 @@ $multipleItemMetadata = array();
 // item will remain quite small.
 while( loop_items() ) {
    $item = get_current_item();
-
-   $itemMetadata = array();
-
-   $itemMetadata['id'] = item( 'id' );
-   $itemMetadata['title'] = html_entity_decode(
-      strip_formatting( item( 'Dublin Core', 'Title' ) ) );
-
-   // Add location information if there is any available.
-   $location = get_db()->getTable(
-      'Location' )->findLocationByItem( $item, true );
-   if( $location ) {
-      $itemLatitude = $location['latitude'];
-      $itemLongitude = $location['longitude'];
-
-      $itemMetadata = array_merge( $itemMetadata,
-         array(
-            'latitude' => $itemLatitude,
-            'longitude' => $itemLongitude,
-         )
-      );
-
-      /* DISABLED: I don't know where this function comes from.
-      if( $itemLatitude && $itemLongitude ) {
-         $itemMetadata['distance_away_miles'] = geocode_measure_distance(
-            $_GET['latitude'], $_GET['longitude'],
-            $itemLatitude, $itemLongitude );
-      }
-       */
+   $location = get_db()->getTable('Location' )->findLocationByItem( $item, true );
+   
+   if((item('public'))&&($location)){ // make sure the item is public and has location data
+   
+	   $itemMetadata = array();
+	
+	   $itemMetadata['id'] = item( 'id' );
+	   $itemMetadata['title'] = html_entity_decode(
+	      strip_formatting( item( 'Dublin Core', 'Title' ) ) );
+	
+	      $itemLatitude = $location['latitude'];
+	      $itemLongitude = $location['longitude'];
+	
+	      $itemMetadata = array_merge( $itemMetadata,
+	         array(
+	            'latitude' => $itemLatitude,
+	            'longitude' => $itemLongitude,
+	         )
+	      );
+	  
+	   array_push($multipleItemMetadata, $itemMetadata);
+   
    }
-
-   array_push($multipleItemMetadata, $itemMetadata);
 }
 
 $metadata = array(
--- MobileJson/views/shared/tours/show.mjson.php
+++ MobileJson/views/shared/tours/show.mjson.php
@@ -3,17 +3,18 @@
 // Add enumarations of the ordered items in this tour.
 $items = array();
 foreach( $tour->Items as $item ) {
-$location = geolocation_get_location_for_item($item->id, true);
-   $item_metadata = array(
-      'id'     => $item->id,
-      'title'  => $this->itemMetadata( $item, 'Dublin Core', 'Title' ),
-      'latitude' => $location['latitude'],
-      'longitude' => $location['longitude']
-   );
-
-   array_push( $items, $item_metadata );
+	$location = geolocation_get_location_for_item($item->id, true);
+	if( ($item->public)&&($location)){	// make sure item is public and has location data
+	   $item_metadata = array(
+	      'id'     => $item->id,
+	      'title'  => $this->itemMetadata( $item, 'Dublin Core', 'Title' ),
+	      'latitude' => $location['latitude'],
+	      'longitude' => $location['longitude']
+	   );
+	
+	   array_push( $items, $item_metadata );
+	}
 }
-
 // Create the array of data
 $tour_metadata = array(
    'id'           => tour( 'id' ),
