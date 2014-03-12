2bb9f4a41cd56441e55d6bcabce5c3baca042025
Erin <ebellempire@gmail.com>
2014-03-12 13:51:59 -0400

    Files

--- views/shared/items/browse.mjson.php
+++ views/shared/items/browse.mjson.php
@@ -23,6 +23,7 @@ function has_element( $name )
 }
 
 $hasSponsor = has_element( 'Sponsor Name' );
+$storage = Zend_Registry::get('storage');
 
    // If it doesn't have location data, we're not interested.
    $location = get_db()->getTable( 'Location' )->findLocationByItem( $item, true );
@@ -57,8 +58,51 @@ $hasSponsor = has_element( 'Sponsor Name' );
          }
       }
 
+      // Add files (version 2 does full sync immediately)
+      $files = array();
+      foreach( $item->Files as $file )
+      {
+         $path = $file->getWebPath( 'original' );
+         $mimetype = metadata( $file, 'MIME Type' );
 
-   
+         $filedata = array(
+            'id'        => $file->id,
+            'mime-type' => $mimetype,
+            'modified'  => $file->modified );
+
+         $title = metadata( $file, array( 'Dublin Core', 'Title' ) );
+         if( $title )
+         {
+            $filedata[ 'title' ] = strip_formatting( $title );
+         }
+
+         $description = metadata( $file, array( 'Dublin Core', 'Description' ) );
+         if( $description )
+         {
+            $filedata[ 'description' ] = $description;
+         }
+
+         if( $file->hasThumbnail() )
+         {
+            $filedata[ 'thumbnail' ] = $file->getWebPath( 'thumbnail' );
+         }
+
+         if( strpos( $mimetype, 'image/' ) === 0 )
+         {
+            $p = $storage->getPathByType( $file->getStoragePath() );
+            list( $width, $height ) = getimagesize( $p );
+            $filedata[ 'width' ] = $width;
+            $filedata[ 'height' ] = $height;
+         }
+
+         $files[ $path ] = $filedata;
+
+      }
+
+      if( count( $files ) > 0 )
+      {
+         $itemMetadata[ 'files' ] = $files;
+      }
 }
 
 $metadata = array(
