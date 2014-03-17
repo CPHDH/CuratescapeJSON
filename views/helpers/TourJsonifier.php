7936a8dc4e8f5a40c2788af8ae2589131d84488c
Greyson Fischer <greyson@foosoft.us>
2014-03-17 18:44:49 -0400

    Moved JSONification to view helper for consistency (tours)

---
+++ views/helpers/TourJsonifier.php
@@ -0,0 +1,41 @@
+<?php
+
+class MobileJson_View_Helper_TourJsonifier extends Zend_View_Helper_Abstract
+{
+   public function __construct()
+   {
+
+   }
+
+   public function tourJsonifier( $tour )
+   {
+      // Add enumarations of the ordered items in this tour.
+      $items = array();
+      foreach( $tour->Items as $item )
+      {
+         set_current_record( 'item', $item );
+         $location = get_db()->getTable('Location')->findLocationByItem($item, true);
+
+         // If it has a location, we'll build the itemMetadata array and push it to items
+         if($location){
+            $item_metadata = array(
+               'id'          => $item->id,
+               'title'       => metadata( 'item', array( 'Dublin Core', 'Title' ) ),
+               'latitude' 	=> $location['latitude'],
+               'longitude' 	=> $location['longitude']
+                                   );
+            array_push( $items, $item_metadata );
+         }
+
+      }
+
+      // Create the array of data
+      $tour_metadata = array(
+         'id'           => $tour->id,
+         'title'        => $tour->title,
+         'description'  => $tour->description,
+         'items'        => $items );
+
+      return $tour_metadata;
+   }
+}
--- views/shared/tours/show.mjson.php
+++ views/shared/tours/show.mjson.php
@@ -1,32 +1,4 @@
 <?php
 
-// Add enumarations of the ordered items in this tour.
-$items = array();
-foreach( $tour->Items as $item )
-{
-   set_current_record( 'item', $item );
-   $location = get_db()->getTable('Location')->findLocationByItem($item, true);
-   
-   // If it has a location, we'll build the itemMetadata array and push it to items
-   if($location){
-   $item_metadata = array(
-      'id'          => $item->id,
-      'title'       => metadata( 'item', array( 'Dublin Core', 'Title' ) ),
-	  'latitude' 	=> $location['latitude'],
-	  'longitude' 	=> $location['longitude']      
-   );
-   array_push( $items, $item_metadata );
-   }
-  
-}
-
-// Create the array of data
-$tour_metadata = array(
-   'id'           => tour( 'id' ),
-   'title'        => tour( 'title' ),
-   'description'  => tour( 'description' ),
-   'items'        => $items,
-);
-
-// Encode the array we've created.
-echo Zend_Json_Encoder::encode( $tour_metadata );
+$tourMetadata = $this->tourJsonifier( $tour );
+echo Zend_Json_Encoder::encode( $tourMetadata );
