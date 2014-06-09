00d31be059082f2df3dbb4fbfda8c0450c87c12f
Greyson Fischer <greyson@foosoft.us>
2014-06-09 16:49:38 -0400

    Lower quantity of necessary memory...
    This breaks out the json-ification of each item rather than loading
    the entire database into an array and turning the resulting object
    into JSON.  This significantly reduces the amount of memory necessary
    for creating a full list.

--- views/shared/items/browse.mjson.php
+++ views/shared/items/browse.mjson.php
@@ -3,26 +3,27 @@
 // Start with an empty array of item metadata
 $multipleItemMetadata = array();
 
+echo '{"items":[';
+
 // Loop through each item, picking up the minimum information needed.
 // There will be no pagination, since the amount of information for each
 // item will remain quite small.
+$itemCount = 0;
 foreach( loop( 'item' ) as $item )
 {
    // If it doesn't have location data, we're not interested.
    $hasLocation = get_db()->getTable( 'Location' )->findLocationByItem( $item, true );
    if( $hasLocation )
    {
+      if( $itemCount > 0 )
+      {
+         echo ',';
+      }
+
       $itemMetadata = $this->itemJsonifier( $item );
-      array_push( $multipleItemMetadata, $itemMetadata );
+      echo Zend_Json_Encoder::encode( $itemMetadata );
+      $itemCount += 1;
    }
 }
 
-$metadata = array(
-   'items'        => $multipleItemMetadata,
-   'total_items'  => count( $multipleItemMetadata )
-);
-
-// I've heard that the Zend JSON encoder is really slow,
-// if this becomes a problem, use the second line.
-echo Zend_Json_Encoder::encode( $metadata );
-//echo json_encode( $metadata );
+echo '], "total_items":' . $itemCount . '}';
