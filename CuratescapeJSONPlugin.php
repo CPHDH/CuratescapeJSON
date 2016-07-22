5cc8fa5ccdf267dcd06709a369da1d472aff655f
Erin Bell <rhodes@dyn-host-137-148-121-115.csuohio.edu>
2016-07-22 10:31:58 -0400

    removes hook: initialize

--- CuratescapeJSONPlugin.php
+++ CuratescapeJSONPlugin.php
@@ -2,18 +2,10 @@
 
 class CuratescapeJSONPlugin extends Omeka_Plugin_AbstractPlugin
 {
-	protected $_hooks = array(
-		'initialize' );
-
 	protected $_filters = array(
 		'response_contexts',
 		'action_contexts',
 		'items_browse_per_page' );
-
-	public function hookInitialize()
-	{
-		get_view()->addHelperPath( dirname( __FILE__ ) . '/views/helpers', 'CuratescapeJSON_View_Helper_' );
-	}
 	
 	public function filterItemsBrowsePerPage( $perPage ){
 				
