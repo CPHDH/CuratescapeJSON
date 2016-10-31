f1d3aab96fdf5ed8349d5c3f4c6437e5152a10d4
Erin Bell <rhodes@dyn-host-137-148-121-115.csuohio.edu>
2016-10-31 12:33:07 -0400

    removes unused mjsonx output

--- CuratescapeJSONPlugin.php
+++ CuratescapeJSONPlugin.php
@@ -2,6 +2,7 @@
 
 class CuratescapeJSONPlugin extends Omeka_Plugin_AbstractPlugin
 {
+
 	protected $_filters = array(
 		'response_contexts',
 		'action_contexts',
@@ -21,9 +22,6 @@ class CuratescapeJSONPlugin extends Omeka_Plugin_AbstractPlugin
 		$contexts['mobile-json'] = array(
 			'suffix' => 'mjson',
 			'headers' => array( 'Content-Type' => 'application/json','Access-Control-Allow-Origin'=>'*' ) );
-		$contexts['mobile-json-extended'] = array(
-			'suffix' => 'mjsonx',
-			'headers' => array( 'Content-Type' => 'application/json','Access-Control-Allow-Origin'=>'*'  ) );
 		return $contexts;
 	}
 
@@ -36,9 +34,7 @@ class CuratescapeJSONPlugin extends Omeka_Plugin_AbstractPlugin
 			is_a( $controller, 'SimplePages_PageController' ) )
 		{
 			$contexts['browse'][] = 'mobile-json' ;
-			$contexts['browse'][] = 'mobile-json-extended' ;
 			$contexts['show'][] = 'mobile-json' ;
-			$contexts['show'][] = 'mobile-json-extended' ;
 			$contexts['index'][] = 'mobile-json' ;
 		}
 
--- views/shared/items/show.mjsonx.php
+++
@@ -1,4 +0,0 @@
-<?php
-// Get the basic data
-$itemMetadata = $this->itemJsonifier( $item, true );
-echo Zend_Json_Encoder::encode( $itemMetadata );
