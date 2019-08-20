fe5be9c358e907bcafdaac115a3da91ae18131a3
Erin Bell <ebellempire@gmail.com>
2019-08-20 16:02:20 -0400

    fixes: records w/o location in app search results

--- views/shared/search/index.mjson.php
+++ views/shared/search/index.mjson.php
@@ -24,7 +24,26 @@ foreach( loop('search_texts') as $searchText )
 	}else{
 		// Search everything
 		$searchTextsMetadata = $this->searchJsonifier( $searchText );
-		array_push( $multipleItemMetadata, $searchTextsMetadata );
+		if($searchText[ 'record_type' ] == 'Item'){
+			
+			// check to see if item has location
+			if(get_db()->getTable( 'Location' )->findLocationByItem( $searchText->record_id, true )){
+				array_push( $multipleItemMetadata, $searchTextsMetadata );
+			}
+			
+		}elseif($searchText[ 'record_type' ] == 'File'){
+			
+			// check to see if PARENT item has location
+			$parent_id=$searchTextsMetadata['result_parent_id'];
+			if($parent_id && get_db()->getTable( 'Location' )->findLocationByItem( $parent_id, true )){
+				array_push( $multipleItemMetadata, $searchTextsMetadata );
+			}
+				
+					
+		}else{
+			array_push( $multipleItemMetadata, $searchTextsMetadata );			
+		}
+		
 	}
 
 }
